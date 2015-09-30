126158feb34663cdb21ea1c8b97a5f03a7af07d8
Yifan Wu <wyfsendoh@gmail.com>
2015-09-30 10:43:59 -0600

    add random.h

--- dataGeneration.cc
+++ dataGeneration.cc
@@ -1,7 +1,25 @@
 #include <stdio>
+#include <string>
+#include "./utils/random.h"
+
+namespace mltest1 {
+
+class genData {
+
+    /*
+    classes: triangle, half circle, straight line
+    */
+
+    int dimension;
+    int classNumber;
+    int *sampleSize; //length = classNumber
+    float range; // [0,range]
+};
+
+}//namespace mltest1
 
 int main(int argc, char *argv[]) {
-    genData task;
-    int retn = task.genData(argc, argv)
+    mltest1::genData task;
+    int retn = task.gen(argc, argv);
     return retn;
 }
---
+++ utils/random.h
@@ -0,0 +1,18 @@
+#ifndef MLTEST1_UTILS_RANDOM_H_
+#define MLTEST1_UTILS_RANDOM_H_
+
+#include <cmath>
+#include <cstdlib>
+
+namespace mltest1 {
+namespace random {
+
+    void seed(unsigned s){
+        srand(s);
+    }
+
+
+}//namespace random
+}//namespace mltest1
+
+#endif MLTEST1_UTILS_RANDOM_H_
