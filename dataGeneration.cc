16beae130657a9abe87e2ed0ccfd9435e5ef9c3c
Yifan Wu <wyfsendoh@gmail.com>
2015-10-01 09:58:26 -0600

    gen data

--- dataGeneration.cc
+++ dataGeneration.cc
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <cstdio>
 #include <string>
 #include <cstring>
 #include "./utils/random.h"
@@ -12,13 +12,14 @@ namespace mltest1 {
 class GenData {
 
     /*
-    classes: triangle, half circle, straight line
+    classes: triangle, half circle, straight line, pulse ...
     */
 
     int dimension;
     int class_number;
     int sample_size; //number of samples for each class
-    float range; // [0,range]
+    double range; // [0,range]
+    std::string filename;
 
     //default constructor
     GenData() {
@@ -26,27 +27,42 @@ class GenData {
         class_number = 2;
         sample_size = 1000;
         range = 1.0;
+        filename = "samples.csv";
     }
 
     int Run(int argc, char* argv){
         /*
-        argv 1: class number, 2: sample size, 3: dimension, 4: range
+        argv 1: class number, 2: sample size, 3: dimension, 4: range, 5: filename
         to be done: rewrite the configuration in a more flexible way
         */
 
         if (argc != PARAMETER_NUM) {
-            printf("Parameter number does not match.");
+            printf("Parameter number does not match.\n");
             return 1;
         }
         class_number = atoi(argv[1]);
         sample_size = atoi(argv[2]);
         dimension = atoi(argv[3]);
         range = atof(argv[4]);
-        float *data = new float[sample_size*class_number][dimension];
+        filename = std::string(argv[5]);
+        double *data = new double[sample_size*class_number*dimension];
 
+        //generate
+        int sample_idx = 0;
         for (int class_idx = 0; class_idx < class_number; i++) {
             //
+            for (int i = 0; i < sample_size; i++){
+                GenSample(data, sample_idx, dimension, class_idx, range);
+                sample_idx += dimension;
+            }
         }
+        //write
+
+        delete [] data;
+    }
+
+    void GenSample(double *data, int sample_idx, int dimension, int class_idx, double range){
+        //generate one sample
     }
 };
 
